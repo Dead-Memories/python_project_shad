cb669f5f2932dc456e5001ea3085b1c15565b67a
hramovaoi <hramovaoi@mts.ru>
2026-03-20 17:36:00 +0300

    Sellers model added

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -13,7 +13,7 @@ from icecream import ic
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 from src.configurations.settings import settings
-from src.models import books  # noqa
+from src.models import books, sellers  # noqa
 from src.models.base import BaseModel
 from src.models.books import Book  # noqa F401
 
